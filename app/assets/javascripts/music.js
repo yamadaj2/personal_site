5445e349a3927747d6411f20180c5d41d4903c2d
jonathonyamada <jonathon.yamada@gmail.com>
2018-11-17 19:02:23 +0000

    dynamic music embeds

---
+++ app/assets/javascripts/music.js
@@ -0,0 +1,32 @@
+var scIframeElement = document.querySelector('#sc-player');
+var scTrack = SC.Widget(scIframeElement);
+
+$(function(){
+  console.log('music js here');
+
+  playSoundcloudVideo();
+
+  function playSynthesiaVideo() {
+    console.log('clickSynthesiaVideo called');
+  }
+
+  function pauseSynthesiaVideo() {
+    console.log('clickSynthesiaVideo called');
+
+  }
+
+  function pauseYouTubeVideo() {
+    console.log('clickYouTubeVideo called');
+
+  }
+
+  function playSoundcloudVideo() {
+    console.log('playSoundcloudVideo called');
+    scTrack.play();
+  }
+
+  function pauseSoundcloudVideo() {
+    console.log('pauseSoundcloudVideo called');
+    scTrack.pause();
+  }
+});
